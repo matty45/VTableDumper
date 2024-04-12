26eab601d0575e40ee4b1b686d25ccae12011795
Matthew <matthewtoo5@googlemail.com>
2024-04-13 00:06:27 +0100

    Update RTTIDumper.cpp

--- RTTIDumper.cpp
+++ RTTIDumper.cpp
@@ -14,6 +14,7 @@ void check_binary(char* binary_path)
 	kaitai::kstream ks(&input);
 
 	mach_o_t data(&ks);
+	printf("ass %i\n", (int)data.magic());
 };
 
 int main(int argc, char* argv[])
@@ -22,16 +23,27 @@ int main(int argc, char* argv[])
 	if (argc == 1)
 	{
 		printf("This program attempts to dump RTTI info from binary files made using either MSVC or GCC and attempt to make header files from them.\n");
-		printf("Usage: RTTIDumper.exe \"path/to/binary\"\n");
+		printf("Microsoft PE: mspe Mach object file format: macho - More types will be added soon.\n");
+		printf("Usage: RTTIDumper.exe \"path/to/binary\" macho/mspe\n");
 	}
 
-	if (argc == 2) {
+	if (argc == 3) {
+		printf("Attempting to dump \"%s\" with file type \"%s\"\n", argv[1], argv[2]);
+
+		char* file_type = argv[2];
+
+		if (strcmp(file_type, "macho") != 0 && strcmp(file_type, "mspe") != 0)
+		{
+			printf("Invalid file type.\n");
+			return 0;
+		}
+
 		char* binary_path = argv[1];
 		if (std::filesystem::exists(binary_path))
 			check_binary(binary_path);
 	}
-	else if (argc > 2)
-		printf("Invalid number of arguments. Exiting.");
+	else if (argc > 3)
+		printf("Invalid number of arguments. Exiting.\n");
 
 	return 0;
 }
