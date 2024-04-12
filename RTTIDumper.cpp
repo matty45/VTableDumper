43eaa823149af1508a5dd24ff8b01922d634baac
Matthew <matthewtoo5@googlemail.com>
2024-04-12 23:27:05 +0100

    mach_o parsing

--- RTTIDumper.cpp
+++ RTTIDumper.cpp
@@ -1,20 +1,37 @@
 // RTTIDumper.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include <cstdio>
+#include <filesystem>
+#include <fstream>
+#include "kaitai/kaitaistream.h"
+#include "parsers/mach_o/mach_o.h"
 
-int main()
+void check_binary(char* binary_path)
 {
-    std::cout << "Hello World!\n";
+	std::ifstream input(binary_path, std::ios::binary);
+
+	kaitai::kstream ks(&input);
+
+	mach_o_t data(&ks);
+};
+
+int main(int argc, char* argv[])
+{
+	//Print help information if no arguments are supplied.
+	if (argc == 1)
+	{
+		printf("This program attempts to dump RTTI info from binary files made using either MSVC or GCC and attempt to make header files from them.\n");
+		printf("Usage: RTTIDumper.exe \"path/to/binary\"\n");
+	}
+
+	if (argc == 2) {
+		char* binary_path = argv[1];
+		if (std::filesystem::exists(binary_path))
+			check_binary(binary_path);
+	}
+	else if (argc > 2)
+		printf("Invalid number of arguments. Exiting.");
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
